c41037279ccf42f0e05741c9919c27ed17f866f8
ViliamKovac1223 <viliamkovac1223@gmail.com>
2024-11-15 09:20:54 +0100

    add file flag

--- main.go
+++ main.go
@@ -8,33 +8,44 @@ import (
 )
 
 func main() {
-    fundFile := "data.md"
+    fundFile := ""
+
+    for i, arg := range os.Args {
+        if arg == "-f" && len(os.Args) > i + 1 {
+            fundFile = os.Args[i + 1]
+        }
+    }
+
+    if fundFile == "" {
+        fmt.Fprintln(os.Stderr, "You have to define which file to use with -f <file_name>")
+        os.Exit(1)
+    }
 
     // Read data file
     rawData, err := os.ReadFile(fundFile)
     if err != nil {
         log.Fatal(err)
     }
-
     data := string(rawData)
-    fmt.Println(data)
 
+    // Get structured data from string
     var reader dataprocessor.DataReader
     fundData, err := reader.ReadFromString(data)
     if err != nil {
-        fmt.Fprintln(os.Stderr ,err)
+        fmt.Fprintln(os.Stderr, err)
         os.Exit(1)
     }
 
+    // Recalculate all sums in structured data
     fundData.CalcSums()
-    fmt.Println(fundData)
 
+    // Get new updated data in string format
     newStringData, err := reader.UpdateString(data, fundData)
-
     if err != nil {
-        fmt.Fprintln(os.Stderr ,err)
+        fmt.Fprintln(os.Stderr, err)
         os.Exit(1)
     }
 
-    fmt.Println(newStringData)
+    // Write new data to the original file
+    os.WriteFile(fundFile, []byte(newStringData), 0664)
 }
